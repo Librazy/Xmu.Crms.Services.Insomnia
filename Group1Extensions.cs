9aff7a06b745824585d050dfdf99c18876695a46
Librazy <im@librazy.org>
2017-12-19 20:05:17 +0800

    init

---
+++ Group1Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Xmu.Crms.Services.Group1;
+using Xmu.Crms.Shared.Service;
+
+namespace Microsoft.Extensions.DependencyInjection
+{ 
+    public static class Group1Extensions
+    {
+        public static IServiceCollection AddGroup1UserService(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<IUserService, UserService>();
+        }
+    }
+}
